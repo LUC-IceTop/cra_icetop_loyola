f55046b96e77b9bdfc08a112c2a9f5cfb147d4a3
G. Agrawal <gunwati.agrawal@icloud.com>
2024-08-31 17:35:16 -0500

    Update l3_i3_sim_hdf.py
    Added asterix to physics glob location to account for different path in 2018

--- 01_l3_processing/sim_hdf5/l3_i3_sim_hdf.py
+++ 01_l3_processing/sim_hdf5/l3_i3_sim_hdf.py
@@ -34,7 +34,7 @@ fnum = int(sys.argv[3])
 #physics = glob("/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012/SIBYLL2.1/He/12630_v1s/Level3_IC86.2012_SIBYLL2.1_He_12630*.i3.*")
 
 #change this line if you are changing which simulation you are using
-physics = glob("/data/ana/CosmicRay/IceTop_level3/sim/IC86."+year+"/SIBYLL2.3/"+particle+"/"+str(fnum)+"_v1s/Level3_IC86."+year+"_SIBYLL2.3_"+particle+"_"+str(fnum)+"*.i3.*")
+physics = glob("/data/ana/CosmicRay/IceTop_level3/sim/IC86."+year+"/SIBYLL2.3*/"+particle+"/"+str(fnum)+"_v1s/Level3_IC86."+year+"_SIBYLL2.3_"+particle+"_"+str(fnum)+"*.i3.*")
 
 #physics = glob("/data/ana/CosmicRay/IceTop_level3/exp/IC86."+str(run_year)+"/"+str(year)+"/*/Level3_IC86."+str(run_year)+"*0_Subrun*.i3.*")
 
